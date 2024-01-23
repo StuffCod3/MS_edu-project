83a9104198a60d34b2adba4f0a8df30e2b54251f
StuffCod3 <evgeny.mell@yandex.ru>
2024-01-23 17:26:41 +0300

    Update JwtUtils

--- api-gateway/src/main/java/ru/stuff/apigateway/utils/JwtTokenUtil.java
+++ api-gateway/src/main/java/ru/stuff/apigateway/utils/JwtTokenUtil.java
@@ -19,42 +19,23 @@ public class JwtTokenUtil {
 
     @Value("${jwt.secret.access}")
     private String accessSecret;
-    @Value("${jwt.secret.refresh}")
-    private String refreshSecret;
 
-    public String generateAccessToken(String email, Claims claims){
-        final LocalDateTime now = LocalDateTime.now();
-        final Instant accessExpirationInstant = now.plusMinutes(15).atZone(ZoneId.systemDefault()).toInstant();
-        final Date accessExpiration = Date.from(accessExpirationInstant);
-
-        claims.put("exp", accessExpiration);
-
-        return Jwts.builder()
-                .setSubject(email)
-                .setExpiration(accessExpiration)
-                .signWith(SignatureAlgorithm.HS256, accessSecret)
-                .setClaims(claims)
-                .compact();
-    }
-
-    public String generateRefreshToken(String email){
-        final LocalDateTime now = LocalDateTime.now();
-        final Instant refreshExpirationInstant = now.plusDays(30).atZone(ZoneId.systemDefault()).toInstant();
-        final Date refreshExpiration = Date.from(refreshExpirationInstant);
-
-        return Jwts.builder()
-                .setSubject(email)
-                .setExpiration(refreshExpiration)
-                .signWith(SignatureAlgorithm.HS256, refreshSecret)
-                .compact();
+    public Claims getAccessClaims(String token) {
+        return getClaims(token, accessSecret);
     }
 
     public boolean validateAccessToken(String accessToken) {
         return validateToken(accessToken, accessSecret);
     }
 
-    public boolean validateRefreshToken(String refreshToken) {
-        return validateToken(refreshToken, refreshSecret);
+    // PRIVATE
+
+    private Claims getClaims(String token, String secret) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey(secret))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     private boolean validateToken(String token, String secret) {
@@ -78,24 +59,14 @@ public class JwtTokenUtil {
         return false;
     }
 
-    private Claims getClaims(String token, String secret) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSignInKey(secret))
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
     private Key getSignInKey(String secret) {
         byte[] keyBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public Claims getAccessClaims(String token) {
-        return getClaims(token, accessSecret);
-    }
 
-    public Claims getRefreshClaims(String token) {
-        return getClaims(token, refreshSecret);
-    }
+
+
+
+
 }
